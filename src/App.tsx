4160d0413acd049ef8cf555fe2e69612e1aeb69a
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2026-06-08 01:55:01 +0400

    feat: modify app tsx and app routes tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Suspense } from "react"
 import AppRoutes from "./features/routes/AppRoutes"
+import { ThemeApplier } from "./shared/components/theme/ThemeApplier"
 
 function App() {
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
+        {/* theme applier */}
+        <ThemeApplier />
+
+        {/* App Routes */}
         <AppRoutes />
       </Suspense>
     </>
--- src/features/routes/AppRoutes.tsx
+++ src/features/routes/AppRoutes.tsx
@@ -3,8 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 
 // Layouts
 import MainLayout from '../../shared/layouts/MainLayout';
-import { ThemeApplier } from '../../shared/components/theme/ThemeApplier';
-
 // Lazy Routes
 const Home = lazy(() => import("../pages/home/Home"));
 const ErrorPage = lazy(() => import("../pages/error/ErrorPage"));
@@ -12,9 +10,6 @@ const ErrorPage = lazy(() => import("../pages/error/ErrorPage"));
 const AppRoutes:React.FC = () => {
   return (
     <Routes>
-      {/* theme applier */}
-      <ThemeApplier />
-
       {/* general pages */}
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
