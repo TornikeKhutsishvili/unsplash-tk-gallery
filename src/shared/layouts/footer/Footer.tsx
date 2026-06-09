0a4e1621d00d41520ee7841aef7939010475eec9
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2026-06-09 18:03:32 +0400

    feat: Modify modal and footer

--- src/shared/layouts/footer/Footer.tsx
+++ src/shared/layouts/footer/Footer.tsx
@@ -4,7 +4,12 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-white border-t mt-10">
+    <footer
+      className="
+        mt-10 bg-white/80 dark:bg-gray-950/80 backdrop-blur-md border-t
+        border-gray-200/60 dark:border-gray-800/60
+      "
+    >
         <div className="container mx-auto py-4 text-center text-gray-600">
             &copy; {currentYear} Unsplash tk-gallery. All rights reserved.
         </div>
